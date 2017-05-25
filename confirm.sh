b11882c36e7b3f80dc0d8244ff8469eb3266dd9a
tkuchiki <kuchiki.taku@gmail.com>
2017-05-25 18:13:31 +0900

    fix confirm.sh

--- confirm.sh
+++ confirm.sh
@@ -5,4 +5,4 @@ echo
 
 [ "${pw1}" != "${pw2}" ] && echo "Password do not match" && exit 1
 
-php -r "echo crypt('${pw}','\$6\$'.hash('sha512', uniqid(mt_rand(),true))), PHP_EOL;"
+php -r "echo crypt('${pw1}','\$6\$'.hash('sha512', uniqid(mt_rand(),true))), PHP_EOL;"
